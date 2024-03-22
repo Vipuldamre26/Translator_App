1ace3eca0cbee3c26b77016792d2cf55edf83d40
Vipul <vipuldamrepro26@gmail.com>
2024-03-22 15:44:21 +0530

    css part and jsx part done

--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -6,78 +6,67 @@ import codeData from './codeData.js';
 const Translator = () => {
 
 
-    const [ result, setResult ] = useState('');
-    const [ data, setData ] = useState(''); 
-    const [ translateto, setTranslateTo ] = useState('');
+    const [result, setResult] = useState('');
     const ref = useRef();
     const ref1 = useRef();
 
 
-
-    const encodedParams = new URLSearchParams();
-    encodedParams.set('source_language', 'auto');
-    encodedParams.set('target_language', translateto);
-    encodedParams.set('text', data);
-
-    console.log(encodedParams);
-
-    const options = {
-        method: 'POST',
-        url: 'https://text-translator2.p.rapidapi.com/translate',
-        headers: {
-            'content-type': 'application/x-www-form-urlencoded',
-            'X-RapidAPI-Key': '6f07d071e0mshf0f49e48170dc4ep11be79jsn351838f8c4ec',
-            'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
-        },
-        data: encodedParams,
-    };
+    
 
 
-    async function fetchData(){
+    async function fetchData(encodedParams) {
+
+        const options = {
+            method: 'POST',
+            url: 'https://text-translator2.p.rapidapi.com/translate',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+                'X-RapidAPI-Key': '6f07d071e0mshf0f49e48170dc4ep11be79jsn351838f8c4ec',
+                'X-RapidAPI-Host': 'text-translator2.p.rapidapi.com'
+            },
+            data: encodedParams,
+        };
+
+
         try {
             const response = await axios.request(options);
+            console.log(response.data.data.translatedText);
+            setResult(response.data.data.translatedText);
             console.log(response.data);
-            setResult(response.data.translatedText);
         } catch (error) {
             console.error(error);
         }
     }
 
-    useEffect(() => {
-        fetchData();
-    },[])
-
 
     const TranslateData = () => {
-        let refcode = ref.current.value;
-        // let reftext = ref1.current.value;
-        let reftext = document.querySelector('input').value;
-        console.log(reftext);
-        setTranslateTo(ref.current.value);
-        setData(reftext);
-        fetchData();
+        const encodedParams = new URLSearchParams();
+        encodedParams.set('source_language', 'auto');
+        encodedParams.set('target_language', ref.current.value);
+        encodedParams.set('text', ref1.current.value);
+        fetchData(encodedParams);
     }
-    
+
 
 
 
     return (
         <div className='translate'>
-            <h1>Translator</h1>
+            <h1>TRANSLATOR</h1>
             <div className='translator'>
                 <select ref={ref}>
                     {
                         codeData.map((item) => {
-                            return(
-                            <option value={item.code} key={item.id}>{item.name}</option>
+                            return (
+                                <option value={item.code} key={item.id}>{item.name}</option>
                             )
                         })
                     }
                 </select>
                 <input ref={ref1} type='text' placeholder='Enter Text'></input>
-                    
+
                 <button onClick={TranslateData}>Translate</button>
-                
+
             </div>
             <p>{result}</p>
         </div>
